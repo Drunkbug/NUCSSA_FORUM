c7cec15bedfeec32ea067705dbdcce8080abd319
Drunkbug <leyiqiang@gmail.com>
2016-05-13 15:33:26 -0400

    problem: can't load view(white screen)

--- NUCSSA_FORUM/ViewController.swift
+++ NUCSSA_FORUM/ViewController.swift
@@ -7,13 +7,41 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    var webView: WKWebView!//initialize webview
+    
+    // URLs
+    let forumURL = "http://nucssa.org/forum/forum.php"
+    let nucssaURL = "http://nucssa.org/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true//Enable javascript !important!
+        
+        let config = WKWebViewConfiguration()
+        config.preferences = preferences // assign config's preferences to preferences
+        //create an instance of web view
+        webView = WKWebView(frame: view.bounds, configuration: config)// view.bounds:hold the entile screen
+        view.addSubview(webView)
+        
+        webViewLoadURL(forumURL)
+        
     }
+
+    func webViewLoadURL(urlString:String) {
+        if let url = NSURL(string: urlString) {
+            let urlRequest = NSURLRequest(URL: url)
+            webView.loadRequest(urlRequest)
+            
+        
+        }
+    }
+    
+    
 
     
     
@@ -22,6 +50,9 @@ class ViewController: UIViewController {
 //        // Dispose of any resources that can be recreated.
 //    }
 
-
+    //MARK: - target action
+    @IBAction func Change_To_Forum(sender: UISegmentedControl) {
+        
+    }
 }
 
